b25e46cde621c96d0a2035581af2374d23d7829d
LuukNoverraz <luuk.noverraz@coderclass.nl>
2023-05-31 00:30:43 +0200

    Remove old code and add placeholder for creatures

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,3 @@
-// let nightValue = 1
-
-// nightBtn.addEventListener('click', siteTime)
-
-// function siteTime() {
-//     defaultCanvas0.style.filter = "invert(" + nightValue + ")"
-//     if (nightValue == 1) {
-//         cover.style.background = "black"
-//         cover.style.pointerEvents = "all"
-//         nightValue = 0
-//         setTimeout(lightsOut, 3000)
-//     }
-//     else if (nightValue == 0) {
-//         cover.style.background = "transparent"
-//         cover.style.pointerEvents = "none"
-//         nightValue = 1
-//     }
-// }
-
-// function lightsOut() {
-//     siteTime()
-// }
-
 let timeOfDay = 12
 const clockImage = document.getElementById('clockImage');
 
