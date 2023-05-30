e0183481548637f2475e76203a597a61fff16d66
LuukNoverraz <luuk.noverraz@coderclass.nl>
2023-05-31 00:24:54 +0200

    Add site time

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,38 @@
-let nightValue = 1
+// let nightValue = 1
 
-nightBtn.addEventListener('click', siteTime)
+// nightBtn.addEventListener('click', siteTime)
 
-function siteTime() {
-    defaultCanvas0.style.filter = "invert(" + nightValue + ")"
-    if (nightValue == 1) {
-        cover.style.background = "black";
-        cover.style.pointerEvents = "all";
-        nightValue = 0
-        setTimeout(lightsOut, 3000)
-    }
-    else if (nightValue == 0) {
-        cover.style.background = "transparent";
-        cover.style.pointerEvents = "none";
-        nightValue = 1
-    }
+// function siteTime() {
+//     defaultCanvas0.style.filter = "invert(" + nightValue + ")"
+//     if (nightValue == 1) {
+//         cover.style.background = "black"
+//         cover.style.pointerEvents = "all"
+//         nightValue = 0
+//         setTimeout(lightsOut, 3000)
+//     }
+//     else if (nightValue == 0) {
+//         cover.style.background = "transparent"
+//         cover.style.pointerEvents = "none"
+//         nightValue = 1
+//     }
+// }
+
+// function lightsOut() {
+//     siteTime()
+// }
+
+let timeOfDay = 12
+const clockImage = document.getElementById('clockImage');
+
+setInterval(dayNightCycle, 5000)
+clockImage.addEventListener('click', skipTime);
+
+function dayNightCycle() {
+    clockText.innerHTML = (timeOfDay < 10 ? "0" + timeOfDay : timeOfDay) + ":00";
+    timeOfDay = (timeOfDay < 23 ? timeOfDay + 1 : 0);
 }
 
-function lightsOut() {
-    siteTime()
+function skipTime() {
+    clockText.innerHTML = (timeOfDay < 10 ? "0" + timeOfDay : timeOfDay) + ":00";
+    timeOfDay = (timeOfDay < 23 ? timeOfDay + 1 : 0);
 }
