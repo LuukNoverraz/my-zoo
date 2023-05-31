b2f5e0c4015da09bd1cf649a495dca0a6d799e6c
Luuk <luuknoverraz@gmail.com>
2023-05-31 11:24:23 +0200

    Replace getElement with querySelector

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,12 @@
 let timeOfDay = 12
-const clockImage = document.getElementById('clockImage');
 
-setInterval(dayNightCycle, 5000)
-clockImage.addEventListener('click', skipTime);
+const clockImage = document.querySelector('#clockImage');
+const clockTime = document.querySelector('#clockTime');
 
-function dayNightCycle() {
-    clockText.innerHTML = (timeOfDay < 10 ? "0" + timeOfDay : timeOfDay) + ":00";
-    timeOfDay = (timeOfDay < 23 ? timeOfDay + 1 : 0);
-}
+setInterval(zooTime, 5000)
+clockImage.addEventListener('click', zooTime);
 
-function skipTime() {
+function zooTime() {
     clockText.innerHTML = (timeOfDay < 10 ? "0" + timeOfDay : timeOfDay) + ":00";
     timeOfDay = (timeOfDay < 23 ? timeOfDay + 1 : 0);
 }
